b50eccbf22d1495c2fac9a791dd3826e3e1021b0
Abdelsalam Selim <aselim@goldencomm.com>
2023-09-06 11:12:32 +0300

    Added Solution

---
+++ LoggingMongoApp/LoggingMongoApp/Logger/FLogger.cs
@@ -0,0 +1,35 @@
+﻿using Serilog;
+
+namespace LoggingMongoApp.Logger
+{
+    public class FLogger : IFLogger
+    {
+        private static Serilog.ILogger _logger;
+        public FLogger()
+        {
+            _logger = new LoggerConfiguration()
+             .MinimumLevel.Information()
+             .WriteTo.MongoDBBson("mongodb://localhost:27017/LogDB", "LogEntries")
+             .CreateLogger();
+        }
+        public void LogDebug(Exception exception)
+        {
+            _logger.Debug(exception,"Debug Message");
+        }
+
+        public void LogError(Exception exception)
+        {
+            _logger.Error(exception, "Error Message");
+        }
+
+        public void LogInfo(Exception exception)
+        {
+            _logger.Information(exception, "Information Message");
+        }
+
+        public void LogWarn(Exception exception)
+        {
+            _logger.Warning(exception, "Warning Message");
+        }
+    }
+}
---
+++ LoggingMongoApp/LoggingMongoApp/Logger/IFLogger.cs
@@ -0,0 +1,10 @@
+﻿namespace LoggingMongoApp.Logger
+{
+    public interface IFLogger
+    {
+        void LogInfo(Exception exception);
+        void LogWarn(Exception exception);
+        void LogDebug(Exception exception);
+        void LogError(Exception exception);
+    }
+}
